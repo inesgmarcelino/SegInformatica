110b3afb7bc9ce07e1a952e50515f332cdfb0d67
inesgmarcelino <inesgmarcelino@gmail.com>
2022-03-10 14:34:45 +0000

    client reads command line and sends to server

--- ProjSInf/src/myAutent.java
+++ ProjSInf/src/myAutent.java
@@ -1,3 +1,69 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
 public class myAutent {
     
+    public static void main(String[] ags) {
+        System.out.println("servidor: main");
+        myAutent server = new myAutent();
+        server.startServer();
+    }
+
+    public void startServer() {
+        ServerSocket servSock = null;
+
+        try {
+            servSock = new ServerSocket(23456);
+            System.out.println("Servidor iniciado no port 23456");
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(-1);
+        }
+
+        while (true) {
+            try {
+                Socket client = servSock.accept();
+                ServerThread thread = new ServerThread(client);
+                thread.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        // servSock.close();
+    }
+
+    class ServerThread extends Thread {
+        private Socket socket = null;
+
+        ServerThread (Socket client) {
+            socket = client;
+            System.out.println("thread do server para cada cliente");
+        }
+
+        public void run() {
+            try {
+                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+                
+                HashMap<String,String> data = null;
+
+                 try {
+                	 data = (HashMap<String, String>) in.readObject();
+                	 System.out.println(data);
+                 } catch (ClassNotFoundException e) {
+                	 e.printStackTrace();
+                 }
+                 
+                 out.close();
+                 in.close();
+                 socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- ProjSInf/src/myAutentClient.java
+++ ProjSInf/src/myAutentClient.java
@@ -1,3 +1,94 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 public class myAutentClient {
     
+    public static void main(String[] args) {
+    	if (args.length >= 4) {
+    		Socket socket = null;
+    		try {
+    			String host = null;
+    			int port = 0;
+    			int user = 1; //default -> admin
+//    			String password = null;
+//    			List<String> newUser = new ArrayList<String>();
+//    			List<String> files = new ArrayList<String>();
+//    			char option = 0;
+    			HashMap<String,String> data = new HashMap<String,String>();
+    			
+    			//user and address
+        		if (args[0].equals("-u") && args[2].equals("-a")) {
+    				data.put("user", args[1]);
+    				user = Integer.parseInt(args[1]);
+    				host = args[3].substring(0, 9);
+    				port = Integer.parseInt(args[3].substring(10));
+    			} else {
+    				System.out.println("comando inválido");
+    				System.exit(-1);
+    			}
+        		
+        		socket = new Socket(host,port);
+    			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+    			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        		
+        		if (args.length > 4) {
+        			//password
+        			if (args[4].equals("-p")) {
+        				data.put("password", args[5]);
+        				
+        				if (args[6].equals("-c") && user != 1) { //admin access only
+        					System.out.println("comando inválido");
+            				System.exit(-1);
+        				}
+        				
+        				data.put("option", args[6].substring(1));
+        				StringBuilder sb = new StringBuilder();
+        				for (int i = 7; i < args.length-1; i++) {
+        					sb.append(args[i]+";");
+        				}
+        				sb.append(args[args.length-1]);
+        				data.put("option_args", sb.toString());
+        				
+        			} else {
+        				Scanner scn = new Scanner(System.in);
+        				System.out.println("Insere a password: ");
+        				data.put("password",scn.nextLine());
+        				
+        				if (args[4].equals("-c") && user != 1) { //admin access only
+        					System.out.println("comando inválido");
+            				System.exit(-1);
+        				}
+        				
+        				data.put("option", args[4].substring(1));
+        				StringBuilder sb = new StringBuilder();
+        				for (int i = 5; i < args.length-1; i++) {
+        					sb.append(args[i]+";");
+        				}
+        				sb.append(args[args.length-1]);
+        				data.put("option_args", sb.toString());
+        		
+        			}
+        			
+        		}
+        		
+        		out.writeObject(data);
+        		out.close();
+        		in.close();
+        		socket.close();
+    			
+    		} catch (IOException e) {
+    			System.err.println(e.getMessage());
+    			System.exit(-1);
+    		}
+    	
+    	} else {
+    		System.out.println("comando inválido");
+    	}
+    }
 }
